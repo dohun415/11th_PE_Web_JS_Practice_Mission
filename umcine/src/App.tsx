530fa501bebde3edab954a351ee02aaffd0ee5b4
dohun <ksw09129@naver.com>
2026-09-21 21:08:06 +0900

    미니 실습(3) 진행

--- umcine/src/App.tsx
+++ umcine/src/App.tsx
@@ -1,11 +1,36 @@
-export default function App() {
-  const movieTitle = "오디세이";
-  const genre = "모험";
+function Header() {
+  return <>Header</>;
+}
 
+function MovieList() {
   return (
-    <article className="movie-card">
-      <h1>{movieTitle}</h1>
-      <p>장르: {genre}</p>
+//React의 JSX 문법에서는 컴포넌트가 반드시 하나의 부모 요소(Single Root Element)만 반환해야 합니다.
+//이렇게 감싸줘야한다.
+    <> 
+    <MovieCard />
+    <MovieCard />
+    </>
+  );
+}
+
+function MovieTitle() {
+  return <h2>오디세이</h2>;
+}
+
+function MovieCard() {
+  return (
+    <article>
+      <MovieTitle />
+      <p>2026.08.05</p>
     </article>
+  );
+}
+
+export default function App() {
+  return (
+    <main>
+      <Header />
+      <MovieList />
+    </main>
   );
 }
